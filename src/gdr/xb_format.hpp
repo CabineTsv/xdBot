d91b63325775d40d8b662db629f45a1a8439d909
CabineTsv <jimberthubert@gmail.com>
2026-08-15 07:43:58 +0800

    Create xb_format.hpp

---
+++ src/gdr/xb_format.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "gdr.hpp"
+
+#include <cstdint>
+#include <span>
+#include <vector>
+
+namespace xb_format {
+
+bool isXBData(std::span<uint8_t const> data);
+
+std::vector<uint8_t> exportXB(BotReplay const& replay);
+
+gdr::Result<BotReplay> importXB(std::span<uint8_t const> data);
+
+} // namespace xb_format
